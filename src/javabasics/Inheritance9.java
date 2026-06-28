be222bce2b633a06b240ded529f884ead90fafe4
Praveen M <praveenmpravee7242@gmail.com>
2026-06-28 12:53:31 +0530

    new file

--- src/javabasics/Inheritance9.java
+++ src/javabasics/Inheritance9.java
@@ -1,4 +1,33 @@
 package javabasics;
 
-public class Inheritance9 {
-}
+    class Keyboard{
+        int keys = 100;
+
+        String color = "blue";
+
+        public void hitnum(){
+            System.out.println("hit number");
+        }
+        public void pressed(){
+            System.out.println("key pressed");
+        }
+    }
+    class AdvKeyboard extends Keyboard{
+        public void throwit(){
+            super.hitnum();
+            System.out.println(keys);
+            System.out.println("got it");
+        }
+    }
+
+   public class Inheritance9{
+        public static void main(String args[]){
+
+
+            AdvKeyboard key1=new AdvKeyboard();
+            key1.throwit();
+
+
+        }
+    }
+
