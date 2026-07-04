8988fd2cffe7a13734ba20d964be4b1641789889
Praveen M <praveenmpravee7242@gmail.com>
2026-07-04 18:10:25 +0530

    new file

---
+++ src/javabasics/Interface1.java
@@ -0,0 +1,36 @@
+package javabasics;
+
+
+interface Computer
+{
+    void code();
+}
+
+class Laptop implements Computer
+{
+    public void code(){
+        System.out.println("laptop using");
+    }
+}
+class Desktop implements Computer{
+    public void code(){
+        System.out.println("using Desktop");
+    }
+}
+class Developer{
+    public void devapp(Computer c)
+    {
+        c.code();
+    }
+}
+
+public class Interface1 {
+    public static void main(String[] args){
+        Computer lap= new Laptop();
+        Computer desk=new Desktop();
+        Developer praveen = new Developer();
+        praveen.devapp(desk);
+
+
+    }
+}
