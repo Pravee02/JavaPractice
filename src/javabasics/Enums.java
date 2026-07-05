10727f19248e3e7f614b2f79bf00c85be646bd1e
Praveen M <praveenmpravee7242@gmail.com>
2026-07-05 15:46:38 +0530

    new file

--- src/javabasics/Enums.java
+++ src/javabasics/Enums.java
@@ -5,8 +5,16 @@ enum Status{
 public class Enums {
     public static void main(String[] args){
         int A= 10;
-        Status s= Status.Success;
-      //  Status s= Status.Failed; // not allowed beacues that word not in enum class
+        Status s= Status.running;
+       //Status s= Status.Failed; // not allowed beacues that word not in enum class
         System.out.println(s);
+       System.out.println(s.ordinal());
+        Status[] ss= Status.values();
+        for(Status a : ss)
+        {
+            System.out.println(a+" :"+ a.ordinal());
+        }
+
+
     }
 }
