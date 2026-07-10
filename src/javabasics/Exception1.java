fa787f8cc80b5546fcf5ed6b7d96069c0cf45951
Praveen M <praveenmpravee7242@gmail.com>
2026-07-10 21:56:18 +0530

    new file

---
+++ src/javabasics/Exception1.java
@@ -0,0 +1,33 @@
+package javabasics;
+
+import java.lang.reflect.Array;
+
+public class Exception1 {
+    public static void main(String[] args){
+            int i = 0;
+            int j=0;
+            int num[]=new int[6];
+
+            try{
+                j=10/0;
+               // System.out.println(j);
+                System.out.println(num[0]);
+            }
+            catch(ArrayIndexOutOfBoundsException e)
+            {
+                System.out.println(e);
+            }
+            catch(ArithmeticException  e)
+            {
+                j=10/5;
+                //System.out.println("cannot divisible by 0");
+            }
+
+
+        System.out.println(j);
+
+
+
+
+    }
+}
